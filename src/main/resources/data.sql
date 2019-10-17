fd616f0d4e9716d237cce2fa41c275045daed20f
micper8503 <michaela-perezp@unilibre.edu.co>
2019-10-17 10:42:50 -0500

    actualizacion3

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT INTO user1 (name, countryid) VALUES (1,'Richard_feynman', '2');
 INSERT INTO user1 (name, countryid) VALUES (2,'Carl_Sagan', '2');
 INSERT INTO user1 (name, countryid) VALUES (3,'Marie_Curie', '1');
 INSERT INTO user1 (name, countryid) VALUES (4,'Johannes_Kepler', '3');
-INSERT INTO user1 (name, countryid) VALUES (5,'J_Kepler', '3');
+INSERT INTO user1 (name, countryid) VALUES (5,'J_Kepler', '4');
